3ddf359d6207e9a5f7234de9e554ee6ca42d48b7
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-24 18:33:46 +0500

    Add project files.

---
+++ AddTwoNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class AddTwoNumber
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public string Add(string num1, string num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumber addTwoNumber = new AddTwoNumber();
+            var result = addTwoNumber.Add(12, 13);
+            Console.WriteLine(result);
+            Console.WriteLine("---------");
+
+            var result1 = addTwoNumber.Add(12.25m, 12.75m);
+            Console.WriteLine(result1);
+            Console.WriteLine("------");
+
+            var result2 = addTwoNumber.Add("hello ", "world");
+            Console.WriteLine(result2);
+
+
+            
+        }
+    }
+}
